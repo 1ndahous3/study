6ff1fedc8e1bb4bb13cd77e879159f60a651179b
R0maNNN <dudin.roman@list.ru>
2015-10-10 14:58:56 +0300

    java commit

---
+++ java/java2/test/src/test/test.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class test {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> arr = new ArrayList<>(0);
+		arr.add(2);
+	}
+
+}
