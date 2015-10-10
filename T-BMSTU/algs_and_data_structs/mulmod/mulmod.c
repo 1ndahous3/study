fa252110b0b41e6a6a78bcede1cc7a4453bcf831
R0maNNN <-_->
2015-10-10 21:56:18 +0300

    Signed-off-by: R0maNNN <-_->

--- T-BMSTU/algs_and_data_structs/mulmod/mulmod.c
+++ T-BMSTU/algs_and_data_structs/mulmod/mulmod.c
@@ -2,14 +2,14 @@
 #include <math.h>
 
 int main() {
-	char i, isAddOverflow, isMulOverflow;
-	long k = 0, a, b, m, b_temp, z, y;
+	char i, isPosExist, isAddOverflow, isMulOverflow;
+	long k = 0, a, b, m, y, z;
 	scanf("%ld %ld\n%ld\n", &a, &b, &m);
 	for (i = 63; i >= 0; i--) {
-		b_temp = ((long) powl(2, i) & b) == (long) powl(2, i);
+		isPosExist = ((long) powl(2, i) & b) == (long) powl(2, i);
 		isMulOverflow = k << 1 < k;
 		z = isMulOverflow ? (k % m) * (2 % m) : k * 2;
-		y = a * b_temp;
+		y = a * isPosExist;
 		isAddOverflow = (z * y) < y && (z * y) < z;
 		k = isAddOverflow ? (z % m) + (y % m) : z + y;
 	}
