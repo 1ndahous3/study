042fbf9991e1185acc7280ec86959c60ed78b89c
R0maNNN <roman@TStorm>
2016-02-05 03:55:10 +0300

    Signed-off-by: R0maNNN <roman@TStorm>

---
+++ T-BMSTU/prog_langs_and_methods/Kth/src/Kth.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class calc {
+
+	static long number(long count) {
+		long i = 1, j = 10, m = 9, num, key;
+		count++;
+
+		while (count - i * m > 0) {
+			count -= i++ * m;
+			j *= 10;
+			m *= 10;
+		}
+		j /= 10;
+		key = count % i;
+		num = j + count / i;
+
+		return (key == 0 ? --num : num / (long) Math.pow(10, i - key)) % 10;
+	}
+}
+
+public class Kth {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println(calc.number(in.nextLong()));
+	}
+}
