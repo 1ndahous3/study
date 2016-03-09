f42d5d7fca0bd14d2d3bb3aab82eb7de881d9e94
RomaNNN@ <-_->
2016-03-10 00:58:50 +0300

    refactoring

--- T-BMSTU/discrete_math/Mars/src/Mars.java
+++ T-BMSTU/discrete_math/Mars/src/Mars.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 import java.util.TreeSet;
 
 public class Mars {
-	private ArrayList<Integer> free_persons;
-	private TreeSet<Integer> group_one, group_two, team_one, team_two;
-	private PriorityQueue<Integer> queue;
-	private boolean[][] mtrx;
-	private boolean[] checked;
-	private int n;
+	private final ArrayList<Integer> free_persons;
+	private TreeSet<Integer> team_one, team_two;
+	private final PriorityQueue<Integer> queue;
+	private final boolean[][] mtrx;
+	private final boolean[] checked;
+	private final int n;
 
 	public Mars(int nel, boolean _mtrx[][]) {
 		n = nel;
@@ -18,19 +18,11 @@ public class Mars {
 		checked = new boolean[nel];
 		team_one = new TreeSet<>();
 		team_two = new TreeSet<>();
-		reset_groups();
-
 		queue = new PriorityQueue<>();
-
 		free_persons = new ArrayList<>();
 		for (int i = 0; i < n; i++) {
 			free_persons.add(i);
 		}
-	}
-
-	void reset_groups() {
-		group_one = new TreeSet<>();
-		group_two = new TreeSet<>();
 	}
 
 	private void addToGroup(TreeSet<Integer> group_foo, int i) {
@@ -40,23 +32,24 @@ public class Mars {
 		}
 	}
 
-	private boolean pushPerson(TreeSet<Integer> group_foo, TreeSet<Integer> group_bar, int i, int j) {
-		if (group_foo.contains(j)) {
+	private boolean pushPerson(TreeSet<Integer> group_foo, TreeSet<Integer> group_bar, int i) {
+		if (group_foo.contains(i)) {
 			return false;
-		} else if (!checked[j]) {
-			addToGroup(group_bar, j);
-			if (!queue.contains(j))
-				queue.add(j);
+		} else if (!checked[i]) {
+			addToGroup(group_bar, i);
+			if (!queue.contains(i)) {
+				queue.add(i);
+			}
 		}
 
 		return true;
 	}
 
-	private boolean splitTeams(int i, int j) {
+	private boolean splitTeams(TreeSet<Integer> group_one, TreeSet<Integer> group_two, int i, int j) {
 		if (group_one.contains(i)) {
-			return pushPerson(group_one, group_two, i, j);
+			return pushPerson(group_one, group_two, j);
 		} else if (group_two.contains(i)) {
-			return pushPerson(group_two, group_one, i, j);
+			return pushPerson(group_two, group_one, j);
 		} else {
 			addToGroup(group_one, i);
 			addToGroup(group_two, j);
@@ -68,20 +61,8 @@ public class Mars {
 		return true;
 	}
 
-	private boolean checkPerson(int i) {
-		for (int j = 0; j < n; j++) {
-			if (mtrx[i][j]) {
-				if (!splitTeams(i, j))
-					return false;
-			}
-		}
-
-		return checked[i] = true;
-	}
-
-	private boolean combinate(ArrayList<SimpleEntry<TreeSet<Integer>, TreeSet<Integer>>> groups) {
-		long k = 0;
-		TreeSet<Integer> group_min = getComb(groups, k), group_max = getComb(groups, ~k), group_a, group_b;
+	private void combine(ArrayList<SimpleEntry<TreeSet<Integer>, TreeSet<Integer>>> groups) {
+		TreeSet<Integer> group_min = getComb(groups, 0), group_max = getComb(groups, ~0), group_a, group_b;
 		pushFreePersons(group_min, group_max, free_persons);
 
 		int diff = Math.abs(group_min.size() - group_max.size());
@@ -101,20 +82,28 @@ public class Mars {
 				if (compareCombs(group_min, group_a)) {
 					group_min = group_a;
 					group_max = group_b;
-					k = i;
 				}
 				if (compareCombs(group_min, group_b)) {
 					group_min = group_b;
 					group_max = group_a;
-					k = i;
 				}
 			}
 		}
 
 		team_one = group_min;
 		team_two = group_max;
+	}
 
-		return true;
+	private boolean checkPerson(TreeSet<Integer> group_one, TreeSet<Integer> group_two, int i) {
+		for (int j = 0; j < n; j++) {
+			if (mtrx[i][j]) {
+				if (!splitTeams(group_one, group_two, i, j)) {
+					return false;
+				}
+			}
+		}
+
+		return checked[i] = true;
 	}
 
 	private static TreeSet<Integer> getComb(ArrayList<SimpleEntry<TreeSet<Integer>, TreeSet<Integer>>> groups, long k) {
@@ -144,19 +133,21 @@ public class Mars {
 
 	private ArrayList<SimpleEntry<TreeSet<Integer>, TreeSet<Integer>>> getBipartiteGraph() {
 		ArrayList<SimpleEntry<TreeSet<Integer>, TreeSet<Integer>>> groups = new ArrayList<>();
+		TreeSet<Integer> group_one = new TreeSet<>(), group_two = new TreeSet<>();
 
 		for (int i = 0; i < n;) {
 			if (!queue.isEmpty()) {
-				if (!checkPerson(queue.remove())) {
+				if (!checkPerson(group_one, group_two, queue.remove())) {
 					return null;
 				}
 				continue;
-			} else if (!checked[i] && !checkPerson(i)) {
+			} else if (!checked[i] && !checkPerson(group_one, group_two, i)) {
 				return null;
 			}
 			if (queue.isEmpty() && !group_one.isEmpty()) {
 				groups.add(new SimpleEntry<>(group_one, group_two));
-				reset_groups();
+				group_one = new TreeSet<>();
+				group_two = new TreeSet<>();
 			}
 			i++;
 		}
@@ -164,8 +155,8 @@ public class Mars {
 		return groups;
 	}
 
-	private static boolean pushFreePersons(TreeSet<Integer> group_one, TreeSet<Integer> group_two,
-			ArrayList<Integer> free) {
+	private static void pushFreePersons(TreeSet<Integer> group_one, TreeSet<Integer> group_two,
+										ArrayList<Integer> free) {
 		ArrayList<Integer> free_loc = new ArrayList<>(free);
 		int n_one = 0, free_n = free_loc.size(), diff = group_two.size() - group_one.size();
 		if (diff < 0) {
@@ -179,8 +170,6 @@ public class Mars {
 			group_one.add(free_loc.remove(0));
 		}
 		group_two.addAll(free_loc);
-
-		return true;
 	}
 
 	private boolean getTeams() {
@@ -189,7 +178,7 @@ public class Mars {
 			return false;
 		}
 
-		combinate(independent_groups);
+		combine(independent_groups);
 
 		return true;
 	}
@@ -202,16 +191,14 @@ public class Mars {
 
 		for (int i = 0; i < nel; i++) {
 			for (int j = 0; j < nel; j++) {
-				mtrx[i][j] = (in.next().equals("-") ? false : true);
+				mtrx[i][j] = !in.next().equals("-");
 			}
 		}
 
 		Mars res = new Mars(nel, mtrx);
 
 		if (res.getTeams()) {
-			for (int i : res.team_one) {
-				System.out.print((i + 1) + " ");
-			}
+			res.team_one.forEach(i -> System.out.println((i + 1) + " "));
 		} else {
 			System.out.println("No solution");
 		}
