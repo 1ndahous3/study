bf04bef0196b0502aa170b1bc1a108cd196ec792
RomaNNN <-_->
2016-03-11 22:15:24 +0300

    refactoring (java 1.8)

--- T-BMSTU/prog_langs_and_methods/MaxNum/src/MaxNum.java
+++ T-BMSTU/prog_langs_and_methods/MaxNum/src/MaxNum.java
@@ -1,31 +1,19 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
-
-class MyCompare implements Comparable<MyCompare> {
-	public String line;
-
-	public MyCompare(String line) {
-		this.line = line;
-	}
-
-	public int compareTo(MyCompare num) {
-		return (int) (Long.parseLong(num.line + line) - Long.parseLong(line + num.line));
-	}
-}
 
 public class MaxNum {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int i = in.nextInt();
-		MyCompare[] yolo = new MyCompare[i];
+		int nel = in.nextInt();
+		ArrayList<Long> nums = new ArrayList<>();
 
-		for (int j = 0; j < i; j++)
-			yolo[j] = new MyCompare(in.next());
+        for (int i = 0; i < nel; i++)
+            nums.add(in.nextLong());
 
-		Arrays.sort(yolo);
-
-		for (MyCompare x : yolo)
-			System.out.print(x.line);
+        nums
+                .stream()
+                .sorted((a, b) -> Long.compare(Long.parseLong(b.toString() + a.toString()), Long.parseLong((a.toString() + b.toString()))))
+                .forEach(System.out::print);
 	}
 }
