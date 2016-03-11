1c10571f902ac731a3d407932af9c31cc3ee5ba6
RomaNNN <-_->
2016-03-12 01:10:23 +0300

    refactoring

--- T-BMSTU/prog_langs_and_methods/Element/src/Element.java
+++ T-BMSTU/prog_langs_and_methods/Element/src/Element.java
@@ -1,25 +1,25 @@
 public class Element<T> {
-    public Element<T> pntr;
-    private T val;
-    
-    public Element(T x) {
-        pntr = this;
-        val = x;
-    }
-    
-    public T x() {
-        return val;
-    }
-    
-    public boolean equivalent(Element<T> elem) {
-        return (this.GetTop().equals(elem.GetTop()));
-    }
-        
-    public void union(Element<T> elem) {
-        this.GetTop().pntr = elem.GetTop();
-    }
-    
-    private Element<T> GetTop() {
-    	return pntr.equals(this) ? pntr : (pntr = pntr.GetTop());
+	public Element<T> pntr;
+	private T val;
+
+	public Element(T x) {
+		pntr = this;
+		val = x;
+	}
+
+	public T x() {
+		return val;
+	}
+
+	public boolean equivalent(Element<T> elem) {
+		return GetTop().equals(elem.GetTop());
+	}
+
+	public void union(Element<T> elem) {
+		this.GetTop().pntr = elem.GetTop();
+	}
+
+	private Element<T> GetTop() {
+		return equals(pntr) ? pntr : (pntr = pntr.GetTop());
 	}
 }
