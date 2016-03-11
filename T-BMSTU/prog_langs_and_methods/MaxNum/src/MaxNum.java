69054b061550c93d5665ceb9d561e954bf6b96a5
RomaNNN <-_->
2016-03-12 01:44:30 +0300

    spaces & tabs

--- T-BMSTU/prog_langs_and_methods/MaxNum/src/MaxNum.java
+++ T-BMSTU/prog_langs_and_methods/MaxNum/src/MaxNum.java
@@ -8,12 +8,12 @@ public class MaxNum {
 		int nel = in.nextInt();
 		ArrayList<Long> nums = new ArrayList<>();
 
-        for (int i = 0; i < nel; i++)
-            nums.add(in.nextLong());
+		for (int i = 0; i < nel; i++)
+			nums.add(in.nextLong());
 
-        nums
-                .stream()
-                .sorted((a, b) -> Long.compare(Long.parseLong(b.toString() + a.toString()), Long.parseLong((a.toString() + b.toString()))))
-                .forEach(System.out::print);
+		nums
+				.stream()
+				.sorted((a, b) -> Long.compare(Long.parseLong(b.toString() + a.toString()), Long.parseLong((a.toString() + b.toString()))))
+				.forEach(System.out::print);
 	}
 }
