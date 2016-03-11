3056bad42f23b3d3f5f838e6ae3cf238939cac01
RomaNNN <-_->
2016-03-11 23:15:37 +0300

    refactoring

--- T-BMSTU/prog_langs_and_methods/MinDist/src/MinDist.java
+++ T-BMSTU/prog_langs_and_methods/MinDist/src/MinDist.java
@@ -1,32 +1,27 @@
 import java.util.Scanner;
 
-class StringLoop {
+public class MinDist {
 	public static int getDiff(String S, char x, char y) {
-		int a = 0x7fffffff, b = 0x7fffffff, diff = 0x7fffffff;
-		
+		int a = Integer.MAX_VALUE, b = Integer.MAX_VALUE, diff = Integer.MAX_VALUE;
+
 		for (int i = 0; i < S.length(); i++) {
 			if (S.charAt(i) == x) {
 				a = i;
-				if (Math.abs(a - b) < diff)
-					diff = Math.abs(a - b);
-			}
-			if (S.charAt(i) == y) {
+				diff = Math.min(Math.abs(a - b), diff);
+			} else if (S.charAt(i) == y) {
 				b = i;
-				if (Math.abs(a - b) < diff)
-					diff = Math.abs(a - b);
+				diff = Math.min(Math.abs(a - b), diff);
 			}
 		}
-		
+
 		return diff - 1;
 	}
-}
 
-public class MinDist {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		String s = in.nextLine();
 		char x = in.next().charAt(0), y = in.next().charAt(0);
-		System.out.println(StringLoop.getDiff(s, x, y));
+		System.out.println(getDiff(s, x, y));
 	}
 }
