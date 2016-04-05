04ca72546b0b69e2f9cf54b749fadfe22ed21453
RomaNNN <-_->
2016-04-06 00:39:13 +0300

    refactoring (mixin template)

--- dlang/binary_search.d
+++ dlang/binary_search.d
@@ -1,45 +1,32 @@
-import
-    std.stdio,
-    std.conv;
+import std.stdio, std.conv;
 
-template binsearch(string compare_sym) {
-    static assert(compare_sym == ">" || compare_sym == "<", ` '<' (ascending) or '>' (descending) expected `);
+mixin template binSearch(T, alias compare) {
+	enum search(T[] arr, T elem) {
+		int mid, min = 0, max = to!int(arr.length) - 1;
 
-    static if (compare == "<") {
-        enum dir_compare = "<";
-        enum rev_compare = ">";
-    } else {
-        enum dir_compare = ">";
-        enum rev_compare = "<";
-    }
+		while (min <= max) {
+			mid = (min + max) / 2;
+			if (compare(arr[mid], elem)) {
+				min = mid + 1;
+			} else if (!compare(arr[mid], elem) && arr[mid] != elem) {
+				max = mid - 1;
+			} else {
+				return mid;
+			}
+		}
 
-    enum binsearch =
-    `function (int[] arr, int elem) {
-        int mid, min = 0, max = arr.length.to!int - 1, res = -1;
-        while (min <= max) {
-        	mid = (min + max) / 2;
-            if (arr[mid]` ~ dir_compare ~ `elem) {
-                min = mid + 1;
-            } else if (arr[mid]` ~ rev_compare ~ `elem) {
-                max = mid - 1;
-            } else {
-                res = mid;
-                break;
-            }
-        }
-        return res;
-    }`;
+		return -1;
+	}
 }
 
-void main(string[] args)
-{
-    int[] a = [1, 2, 3, 4, 5];
-    int[] b = [5, 4, 3, 2, 1];
+void main(string[] args) {
+	int[] a = [1, 2, 3, 4, 5];
+	int[] b = [5, 4, 3, 2, 1];
 
-    auto ascending_binsearch = mixin(binsearch!("<"));
-    auto descending_binsearch = mixin(binsearch!(">"));
+	mixin binSearch!(int, (a, b) => a < b) ascending;
+	mixin binSearch!(int, (a, b) => a > b) descending;
 
-    ascending_binsearch(a, 4).writeln();
-    descending_binsearch(b, 3).writeln();
-    descending_binsearch(b, 7).writeln();
+	ascending.search(a, 4).writeln();
+	descending.search(b, 3).writeln();
+	descending.search(b, 7).writeln();
 }
