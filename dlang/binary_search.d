ec40078a6a7e6e7a8720d64843e8b866bea46d38
R0maNNN <roman@TStorm>
2016-06-10 18:13:34 +0300

    refactoring (alias & template)

--- dlang/binary_search.d
+++ dlang/binary_search.d
@@ -1,8 +1,8 @@
 import std.stdio, std.conv;
 
-mixin template binSearch(T, alias compare) {
-	enum search(T[] arr, T elem) {
-		int mid, min = 0, max = to!int(arr.length) - 1;
+template binSearch(T, alias compare) {
+	long binSearch(T[] arr, T elem) {
+		long mid, min = 0, max = to!long(arr.length) - 1;
 
 		while (min <= max) {
 			mid = (min + max) / 2;
@@ -23,10 +23,10 @@ void main(string[] args) {
 	int[] a = [1, 2, 3, 4, 5];
 	int[] b = [5, 4, 3, 2, 1];
 
-	mixin binSearch!(int, (a, b) => a < b) ascending;
-	mixin binSearch!(int, (a, b) => a > b) descending;
+	alias binSearch!(int, (a, b) => a < b) ascending;
+	alias binSearch!(int, (a, b) => a > b) descending;
 
-	ascending.search(a, 4).writeln();
-	descending.search(b, 3).writeln();
-	descending.search(b, 7).writeln();
+	ascending(a, 4).writeln();
+	descending(b, 3).writeln();
+	descending(b, 7).writeln();
 }
